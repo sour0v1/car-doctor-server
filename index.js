05deb71d05f4d4daac8ff9de94ca5c73a04c1cb0
Sourov <sourovdas730@gmail.com>
2024-05-11 16:07:17 +0600

    create delete api

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
         const database = client.db("carDoctor");
         // collections
         const serviceCollection = database.collection('services');
+        const bookingCollection = database.collection('bookings')
         // api
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
@@ -42,6 +43,28 @@ async function run() {
                 projection: { title: 1, price: 1, img: 1 },
             };
             const result = await serviceCollection.findOne(query, options);
+            res.send(result);
+        })
+        // bookings
+        app.get('/bookings', async(req, res) =>{
+            console.log(req.query.email);
+            let query = {};
+            if(req.query?.email){
+                query = {email : req.query.email}
+            }
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result);
+        })
+        app.post('/bookings', async(req, res) =>{
+            const bookingDetails = req.body;
+            console.log(bookingDetails);
+            const result = await bookingCollection.insertOne(bookingDetails);
+            res.send(result);
+        })
+        app.delete('/booking/:id', async (req, res) =>{
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)}
+            const result = await bookingCollection.deleteOne(query);
             res.send(result);
         })
 
